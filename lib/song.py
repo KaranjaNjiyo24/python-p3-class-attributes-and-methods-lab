e3bc31f30a3d3725ec8e3d06da85bcec0af6fc9f
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-13 14:20:59 +0300

    Song class and count attribute

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,10 @@
 class Song:
+
+    count = 0
+    all = []
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
     pass
