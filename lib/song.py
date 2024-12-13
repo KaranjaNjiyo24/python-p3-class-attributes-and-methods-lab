cb3993d30a23e5d8ac82f7d7dc95ee7241c9c06f
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-13 15:29:25 +0300

    Class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,10 +1,43 @@
 class Song:
 
     count = 0
-    all = []
+    genres = []
+    artists = []
+    genre_count = {} #dictionary
+    artist_count = {}
     
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-    pass
+        self.add_song_to_count()
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    def add_song_to_count(self):
+        Song.count += 1
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] +=1
+        else: 
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] +=1
+        else:
+            Song.artist_count[self.artist] = 1
+
+    @classmethod
+    def add_to_genres(cls):
+        cls.genres = list(set(cls.genres))
+
+    @classmethod
+    def add_to_artists(cls):
+        cls.artists = list(set(cls.artists))
+    
+    
+    
